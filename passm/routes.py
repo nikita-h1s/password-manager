1f4312a57fc54f0fc7c9ef53ebf07d77af6c4f86
nikita <nikitalinnik22@gmail.com>
2024-11-06 00:30:17 +0200

    Implement rout for viewing passwords

--- passm/routes.py
+++ passm/routes.py
@@ -4,9 +4,14 @@ from .db import get_db
 main = Blueprint('main', __name__)
 
 
-@main.route('/', methods=['GET', 'POST'])
-@main.route('/home', methods=['GET', 'POST'])
+@main.route('/')
+@main.route('/home')
 def home():
+    return render_template('passwords_list.html')
+
+
+@main.route('/resource/new', methods=['GET', 'POST'])
+def add_resource():
     if request.method == 'POST':
         resource_details = request.form
         resource = resource_details.get('name')
@@ -17,15 +22,36 @@ def home():
             cur = db.cursor()
             try:
                 cur.execute("INSERT INTO resource (resource_name) VALUES (%s)", (resource,))
-                cur.execute("INSERT INTO password (encrypted_password) VALUES (%s)", (password,))
+                resource_id = cur.lastrowid
+
+                cur.execute("INSERT INTO password (encrypted_password, resource_id) VALUES (%s, %s)",
+                            (password, resource_id))
                 db.commit()
-                flash('Data added successfully', 'info')
+                flash('Resource added successfully', 'info')
+
+                return redirect(url_for('main.add_resource'))
             except Exception as e:
                 db.rollback()
                 print("An error occurred:", e)
-                flash('Failed to add data', 'danger')
+                flash('Failed to add resource', 'danger')
             finally:
                 cur.close()
+        elif not resource or not password:
+            flash('Please fill out all fields', 'danger')
 
-            return redirect(url_for('main.home'))
-    return render_template('layout.html')
+    return render_template('add_resource.html')
+
+
+@main.route('/password-list')
+def view_passwords():
+    db = get_db()
+    cur = db.cursor()
+    sql_query = """SELECT resource.resource_name, password.encrypted_password FROM resource
+                   INNER JOIN password
+                   ON password.resource_id = resource.resource_id;"""
+    cur.execute(sql_query)
+    resources = cur.fetchall()
+    cur.close()
+    db.close()
+
+    return render_template('passwords_list.html', resources=resources)
