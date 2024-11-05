46a7183da5ecd1a1a6d0df459d7ac5cafd2bec77
nikita <nikitalinnik22@gmail.com>
2024-11-06 00:31:22 +0200

    Add js script and project icons
    Added a js script for displaying bootstrap tooltips on a page.

--- passm/static/js/main.js
+++ passm/static/js/main.js
@@ -1,8 +1,12 @@
 /* global bootstrap: false */
 (function () {
-  'use strict'
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 })()
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
