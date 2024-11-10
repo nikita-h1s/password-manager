ed1f76fb525c69b52b92b696625e835101268395
nikita <nikitalinnik22@gmail.com>
2024-11-11 01:29:41 +0200

    Add utils file for utility functions

--- passm/routes.py
+++ passm/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .db import get_db
+from .utils import retrieve_vaults, get_resources_by_vault
 
 main = Blueprint('main', __name__)
 
@@ -43,7 +44,7 @@ def add_resource():
                 flash('Failed to add resource', 'danger')
             finally:
                 cur.close()
-        elif not resource or not password:
+        else:
             flash('Please fill out all fields', 'danger')
 
     return render_template('add_resource.html', vaults=vault_list)
@@ -95,50 +96,3 @@ def view_vaults():
     vault_list = retrieve_vaults()
 
     return render_template('layout.html', vaults=vault_list)
-
-
-def retrieve_vaults():
-    """Returns a list of vaults"""
-    db = get_db()
-    cur = db.cursor()
-    sql_query = "SELECT vault.vault_id, vault.vault_name, vault.vault_description FROM vault;"
-    cur.execute(sql_query)
-    vaults = cur.fetchall()
-    cur.close()
-    db.close()
-
-    vault_list = []
-    for vault in vaults:
-        vault_dict = {
-            'id': vault[0],
-            'name': vault[1],
-            'description': vault[2]
-        }
-        vault_list.append(vault_dict)
-
-    return vault_list
-
-
-def get_resources_by_vault(vault_id=None):
-    db = get_db()
-    cur = db.cursor()
-    if vault_id:
-        sql_query = """
-                SELECT resource.resource_name, password.encrypted_password 
-                FROM resource
-                INNER JOIN password ON password.resource_id = resource.resource_id
-                INNER JOIN resource_vault ON resource.resource_id = resource_vault.resource_id
-                WHERE resource_vault.vault_id = %s;
-            """
-        cur.execute(sql_query, (vault_id,))
-    else:
-        sql_query = """SELECT resource.resource_name, password.encrypted_password FROM resource
-                           INNER JOIN password
-                           ON password.resource_id = resource.resource_id;"""
-        cur.execute(sql_query)
-
-    resources = cur.fetchall()
-    cur.close()
-    db.close()
-
-    return [{'name': resource[0], 'password': resource[1]} for resource in resources]
