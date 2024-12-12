fafdc0bcb6d4e6547dba3597c2272f688e0eb535
nikita <nikitalinnik22@gmail.com>
2024-12-12 02:59:46 +0200

    Implement functions for password checking
    Created functions for checking for a common password
    and evaluating password strength based on some parameters.

--- passm/utils.py
+++ passm/utils.py
@@ -1,7 +1,7 @@
 import os
 from cryptography.fernet import Fernet
 from .db import get_db
-from datetime import datetime
+import string
 
 
 def retrieve_vaults():
@@ -62,6 +62,7 @@ def get_resources_by_vault(vault_id=None):
 
     return resource_list
 
+
 def encrypt_password(password):
     crypter = Fernet(os.environ.get('ENCRYPTION_KEY'))
     encrypted_password = crypter.encrypt(password.encode('utf-8'))
@@ -72,3 +73,82 @@ def decrypt_password(encrypted_password):
     crypter = Fernet(os.environ.get('ENCRYPTION_KEY'))
     decrypted_password = crypter.decrypt(encrypted_password)
     return decrypted_password.decode('utf-8')
+
+
+def check_common_password(password):
+    # TODO: make a relative path
+    with open(r'D:\Programming\nure\pass-manager\passm\data\common-password.txt', 'r') as f:
+        common = f.read().splitlines()
+
+    if password in common:
+        return True
+
+    return False
+
+
+def password_strength(password):
+    score = 0
+    length = len(password)
+
+    upper_case = any(c.isupper() for c in password)
+    lower_case = any(c.islower() for c in password)
+    special = any(c in string.punctuation for c in password)
+    digits = any(c.isdigit() for c in password)
+
+    characters = [upper_case, lower_case, special, digits]
+
+    if length > 8:
+        score += 1
+    if length > 12:
+        score += 1
+    if length > 17:
+        score += 1
+    if length > 20:
+        score += 1
+
+    score += sum(characters) - 1
+
+    if score < 4:
+        return 'Weak', score
+    elif score == 4:
+        return 'Okay', score
+    elif 4 < score < 6:
+        return 'Good', score
+    else:
+        return 'Strong', score
+
+
+def get_password_stats():
+    """Returns arrays of weak & repeated passwords"""
+    # Fetching a list of passwords
+    db = get_db()
+    cur = db.cursor()
+
+    sql_query = """SELECT password_id, encrypted_password FROM password"""
+    cur.execute(sql_query)
+    password_list = cur.fetchall()
+
+    # Looking for weak passwords
+    weak_password_list = []
+    for idx, p in password_list:
+        decrypted_password = decrypt_password(p)
+        if check_common_password(decrypted_password):
+            weak_password_list.append(idx)
+
+        strength, score = password_strength(decrypted_password)
+
+        if score <= 4:
+            weak_password_list.append(idx)
+
+    # Looking for repeated passwords
+    password_groups = {}
+    for idx, p in password_list:
+        decrypted_password = decrypt_password(p)
+        if decrypted_password in password_groups:
+            password_groups[decrypted_password].append(idx)
+        else:
+            password_groups[decrypted_password] = [idx]
+
+    repeated_password_list = [group for group in password_groups.values() if len(group) > 1]
+
+    return weak_password_list, repeated_password_list
