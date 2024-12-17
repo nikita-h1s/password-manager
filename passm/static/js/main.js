e663ac25cc2c380d9c3020216bf304ceb475b3cb
nikita <nikitalinnik22@gmail.com>
2024-12-18 01:37:02 +0200

    Add new styles and js junction for displaying data

--- passm/static/js/main.js
+++ passm/static/js/main.js
@@ -7,8 +7,10 @@ const initializeTooltips = () => {
 
 // Item information from password list
 const setupResourceClickHandlers = () => {
-    const resourceItems = document.querySelectorAll('.list-group-item');
+    const resourceItems = document.querySelectorAll('.list-resource-item');
     const resourceName = document.querySelector('.resource-name');
+    const resourceUsername = document.querySelector('.resource-username');
+    const resourceEmail = document.querySelector('.resource-email');
     const resourcePassword = document.querySelector('.resource-password');
     const resourceURL = document.querySelector('.resource-url');
     const resourceCreationDate = document.querySelector('.resource-creation-date');
@@ -29,6 +31,8 @@ const setupResourceClickHandlers = () => {
         const resource = resources[resourceId];
         if (resource) {
             resourceName.value = `${resource.name}`;
+            resourceUsername.value = `${resource.username}`;
+            resourceEmail.value = `${resource.email}`;
             resourcePassword.value = `${resource.password}`;
             resourceURL.value = `${resource.resource_url || 'N/A'}`;
             resourceCreationDate.value = `${resource.resource_creation_date}`;
@@ -56,9 +60,10 @@ const setupResourceClickHandlers = () => {
         });
     });
 
+    let selectedResourceId = null;
     if (selectedResourceId) {
         const selectedItem = document.querySelector(
-            `.list-group-item[data-id="${selectedResourceId}"]`
+            `.list-resource-item[data-id="${selectedResourceId}"]`
         );
         if (selectedItem) {
             setActiveItem(selectedItem);
@@ -78,7 +83,7 @@ const setupResourceClickHandlers = () => {
 
             // Set the active item visually
             const activeItem = document.querySelector(
-                `.list-group-item[data-id="${resourceId}"]`
+                `.list-resource-item[data-id="${resourceId}"]`
             );
             if (activeItem) {
                 setActiveItem(activeItem);
@@ -89,21 +94,66 @@ const setupResourceClickHandlers = () => {
 
 
 const editResourcesInformation = () => {
-    document.querySelector('.edit-button').addEventListener('click', function () {
-        document.querySelectorAll('.form-control').forEach(input => {
-            if (!input.classList.contains('resource-exception-date')) {
-                input.disabled = false
+    const editButton = document.querySelector('.edit-button');
+    const saveButton = document.querySelector('.save-button');
+
+    if (editButton) {
+        editButton.addEventListener('click', function () {
+            document.querySelectorAll('.form-control').forEach(input => {
+                if (!input.classList.contains('resource-exception-date')) {
+                    input.disabled = false
+                }
+            });
+            document.querySelector('.edit-button').style.display = 'none';
+            document.querySelector('.save-button').style.display = 'inline-block';
+        });
+    }
+
+    if (saveButton) {
+        document.querySelector('.save-button').addEventListener('click', function () {
+            const form = document.querySelector('.resource-form');
+            const data = new FormData(form);
+
+            form.submit();
+        });
+    }
+};
+
+
+const toggleListVisibility = () => {
+    document.querySelectorAll('.list-resource-stats-item').forEach(button => {
+        button.addEventListener('click', () => {
+            const targetId = button.getAttribute('data-target');
+            if (targetId) {
+                // Hide all buttons
+                document.querySelectorAll('.list-resource-stats-item').forEach(btn => {
+                    btn.classList.add('d-none');
+                });
+
+                // Show the associated list
+                const targetElement = document.getElementById(targetId);
+                if (targetElement) {
+                    targetElement.classList.remove('d-none');
+                } else {
+                    console.error(`Element with ID "${targetId}" not found.`);
+                }
             }
         });
-        document.querySelector('.edit-button').style.display = 'none';
-        document.querySelector('.save-button').style.display = 'inline-block';
     });
 
-    document.querySelector('.save-button').addEventListener('click', function () {
-        const form = document.querySelector('.resource-form');
-        const data = new FormData(form);
-
-        form.submit();
+    // Event delegation for "Back" buttons
+    document.querySelectorAll('.back-to-main').forEach(backButton => {
+        backButton.addEventListener('click', () => {
+            const targetId = backButton.getAttribute('data-target');
+            if (targetId) {
+                // Show the button
+                document.querySelectorAll('.list-resource-stats-item').forEach(btn => {
+                    btn.classList.remove('d-none');
+                });
+                // Hide the associated list
+                document.getElementById(targetId).classList.add('d-none');
+            }
+        });
     });
 };
 
@@ -124,4 +174,5 @@ document.addEventListener('DOMContentLoaded', () => {
     setupResourceClickHandlers();
     editResourcesInformation();
     removeFlashMessages();
+    toggleListVisibility();
 });
